73a0e92f6b24ac7c48e2342630101a31f53e139b
Vitaly Artemev <fleshphp@gmail.com>
2015-12-30 19:08:14 +0300

    Init repository

---
+++ app.js
@@ -0,0 +1,40 @@
+var PORT = 80;
+
+var options = {
+//    'log level': 0
+};
+
+var express = require('express');
+var http = require('http');
+
+var app = express();
+var server = http.createServer(app).listen(PORT);
+var io = require('socket.io').listen(server, options);
+
+app.use('/', express.static(__dirname + '/static'));
+
+io.sockets.on('connection', function (client) {
+    console.log(client.id);
+    
+    client.on('updateServer', function (message) {
+        data = {
+            id: client.id,
+            speed: 256, // movement in pixels per second
+            angle: message.angle,
+            x: message.x,
+            y: message.y,
+            width: message.width,
+            height: message.height
+        };
+        client.broadcast.emit('updateClient', data);
+    });
+    
+    client.on('auth', function (message) {
+        client.emit('auth', client.id);
+    });
+    client.on('disconnect', function () {
+        client.broadcast.emit('delPlane', client.id);
+    });
+});
+
+console.log('Listen 80 port');
